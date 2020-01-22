80e8af74d65a11a504f3476c5ee9714b845f98b1
Agnieszka Ziomek <a.ziomek@nowaera.pl>
2020-01-22 16:47:08 +0100

    clean up

--- src/app/components/flashcards/flashcards.component.ts
+++ src/app/components/flashcards/flashcards.component.ts
@@ -16,33 +16,33 @@ export class FlashcardsComponent implements OnInit {
     this.flashcards = [
       {
         id: 1,
-        question: 'How are you?',
-        answer: 'Fine, thank you!'
+        question: 'Question 1?',
+        answer: 'Answer 1!'
       },
       {
-        id: 2,
-        question: 'How do you do?',
-        answer: 'How do you do?'
+        id: 3,
+        question: 'Question 2?',
+        answer: 'Answer 2!'
       },
       {
         id: 3,
         question: 'Question 3?',
-        answer: 'Answer 3?'
+        answer: 'Answer 3!'
       },
       {
         id: 4,
         question: 'Question 4?',
-        answer: 'Answer 4?'
+        answer: 'Answer 4!'
       },
       {
         id: 5,
         question: 'Question 5?',
-        answer: 'Answer 5?'
+        answer: 'Answer 5!'
       },
       {
         id: 6,
         question: 'Question 6?',
-        answer: 'Answer 6?'
+        answer: 'Answer 6!'
       },
     ];
     this.deck = {
